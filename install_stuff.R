bc70d268d505a62ebdd8f2801986aaa541a51a18
liefeld <liefeld@broadinstitute.org>
2023-02-17 09:40:26 -0800

    Update to latest from JB

--- install_stuff.R
+++ install_stuff.R
@@ -4,15 +4,20 @@ print("- installing")
 install.packages("BiocManager")
 
 install.packages(c("getopt", "optparse" ))
-install.packages('gplots')
+#install.packages('gplots')
 
-BiocManager::install("limma")
+#BiocManager::install("limma")
 BiocManager::install("GenomicRanges")
-BiocManager::install("dplyr")
+#BiocManager::install("dplyr")
 BiocManager::install("TxDb.Hsapiens.UCSC.hg38.knownGene")
 BiocManager::install("org.Hs.eg.db")
 BiocManager::install("Homo.sapiens")
 BiocManager::install("Repitools")
+
+BiocManager::install("Mus.musculus")
+BiocManager::install("TxDb.Mmusculus.UCSC.mm10.knownGene")
+BiocManager::install("TxDb.Mmusculus.UCSC.mm39.refGene")
+BiocManager::install("org.Mm.eg.db")
 
 install.packages("/build/source/OutSplice.tar.gz", repos = NULL, type = "source")
 
